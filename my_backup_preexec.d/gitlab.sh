70949268188023a29474edf7ead6bbad071c551b
Sergio Barrachina Mir <sergio.barrachina@gmail.com>
2022-04-25 16:10:11 +0200

    Extract the gitlab name and version

--- my_backup_preexec.d/gitlab.sh
+++ my_backup_preexec.d/gitlab.sh
@@ -12,10 +12,15 @@ echo "==  GitLab  =="
 # Check required variables
 [ -z "${GITLAB_CONTAINER_NAME}" -o -z "${GITLAB_SRV_DIR}" ] && { echo "Warning: GitLab backup skipped!"; return; }
 
+# Get backup name
+BACKUP_NAME=$(docker exec gitlab grep gitlab-ce /opt/gitlab/version-manifest.txt | replace " " "-")
+[ -z "${BACKUP_NAME}" ] && BACKUP_NAME=$(docker exec gitlab grep gitlab-ee /opt/gitlab/version-manifest.txt | replace " " "-")
+[ -z "${BACKUP_NAME}" ] && BACKUP_NAME="gitlab-unknown"
+
 # Dump GitLab data
 echo "Backing up data from GitLab in '${GITLAB_SRV_DIR}/data/backups/'..."
 echo
-docker exec -t ${GITLAB_CONTAINER_NAME} gitlab-backup create STRATEGY=copy BACKUP=dump GZIP_RSYNCABLE=yes
+docker exec -t ${GITLAB_CONTAINER_NAME} gitlab-backup create STRATEGY=copy BACKUP=${BACKUP_NAME} GZIP_RSYNCABLE=yes
 
 #/----------------------------------
 # Restoring the gitlab backup
