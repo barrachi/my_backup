fbf967537af56f63446183eb9d885629d87128a9
Sergio Barrachina Mir <sergio.barrachina@gmail.com>
2022-05-26 14:50:24 +0200

    Updating 'before_backup.d/postgresql.sh' script (iii)

--- before_backup.d/postgresql.sh
+++ before_backup.d/postgresql.sh
@@ -21,7 +21,7 @@ PGSQL_DIR="${MY_BACKUP_OUTPUT_DIR}/pgsql/${PGSQL_VER}"
 echo "Backing up data from PostgreSQL ${PGSQL_VER} in '${PGSQL_DIR}/'..."
 sudo -u  ${PGSQL_USER} /usr/bin/pg_dumpall | gzip > "${PGSQL_DIR}/pg_dumpall.out.gz"
 PGSQL_SYSTEM_DIR=/etc/postgresql-${PGSQL_VER}  # Gentoo
-[ -d "${PGSQL_DIR}" ] || PGSQL_SYSTEM_DIR==/etc/postgresql/${PGSQL_VER}/main  # Ubuntu
+[ -d "${PGSQL_DIR}" ] || PGSQL_SYSTEM_DIR=/etc/postgresql/${PGSQL_VER}/main  # Ubuntu
 for f in pg_hba.conf pg_ident.conf postgresql.conf;
 do
   cp ${PGSQL_SYSTEM_DIR}/${f} ${PGSQL_DIR}/data/${f}
